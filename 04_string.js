ac6f05ee7dd5ae43573b9296c25acb6039cf2c72
sharmadeeps <deependrasharma36@gmail.com>
2025-03-31 16:22:49 +0545

    string

---
+++ 04_string.js
@@ -0,0 +1,23 @@
+const name = "dipendra"
+const repoCount = 50
+
+//console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String('dipendra')
+
+//console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+
+//console.log(gameName.lenght);
+// console.log(gameName.charAt(3));
+
+// console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0, 4)
+
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+
