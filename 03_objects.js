8b95525be3be4d6f25f70e42ca62843653f00cd1
sharmadeeps <deependrasharma36@gmail.com>
2025-04-01 08:14:51 +0545

    array2

---
+++ 03_objects.js
@@ -0,0 +1,42 @@
+
+// singleton (from constructors)
+
+
+// object literals
+
+const mySymbol = Symbol("key1")  //Symbol Declearation
+
+const jsUser = {
+    name: "dipedra",
+    [mySymbol]: "mykey1",   // Symbol call
+    age: "32",
+    location: "Biratnagar",
+    email: "deeps@gmail.com",
+    isLoggedin: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser[mySymbol]);      //Symbol use , the output will be mykey1
+
+jsUser.email = "dipendra@gmail.com"
+//console.log(jsUser["email"]);
+//Object.freeze(jsUser)
+
+jsUser.email = "deeps@google.com"
+console.log(jsUser["email"]);
+
+// console.log(jsUser);
+
+jsUser.greating = function() {
+    console.log("Hello JS users");
+}
+
+jsUser.greatingTwo = function() {
+    console.log(`Hellow JS user, ${this.name}`);
+    
+}
+
+console.log(jsUser.greating());
+console.log(jsUser.greatingTwo());
---
+++ 04_object.js
@@ -0,0 +1,14 @@
+//Singleton (constructor)
+
+// const tinderuser = new Object()
+
+const tinderuser = {}
+
+tinderuser.id = "123"
+tinderuser.name = "dipendra"
+tinderuser.isLoggedIn = false
+
+
+console.log(tinderuser);
+
+
